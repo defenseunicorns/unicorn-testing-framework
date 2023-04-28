3663ea48ebbff35f34f0963a83ae167eb40ad8c5
ablanchard <andrew.blanchard@leapfrog.ai>
2023-04-28 15:33:46 -0400

    adding timeout to wait script

--- examples/after-deployment-validation-script/validate-podinfo.sh
+++ examples/after-deployment-validation-script/validate-podinfo.sh
@@ -1,4 +1,11 @@
-while [[ $(kubectl get pods -n podinfo -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
-  echo "waiting for pod" && sleep 1
+for i in $(seq 1 14); do
+  echo "waiting for pod..."
+  if [[ $(kubectl get pods -n podinfo -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') = "True" ]]; do
+    echo "found it!"
+    kubectl get all -n podinfo
+    exit 0
+  fi
+  sleep 5
 done
-kubectl get all -n podinfo
+echo "timed out waiting for pod"
+exit 1
